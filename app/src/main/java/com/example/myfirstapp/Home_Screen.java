1a617d0c998f4568ab3feb5235967da24fea7752
nkb2208 <nkb22082006@gmail.com>
2026-08-31 21:39:48 +0700

    add: default list view and customized list view in Home Page

--- app/src/main/java/com/example/myfirstapp/Home_Screen.java
+++ app/src/main/java/com/example/myfirstapp/Home_Screen.java
@@ -3,6 +3,8 @@ package com.example.myfirstapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +12,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class Home_Screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        ArrayList<String> data = new ArrayList<>();
+        for(int i = 1; i< 6; i++){
+            data.add("item " + i);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                data
+        );
+        ListView listView = findViewById(R.id.lstItems);
+        listView.setAdapter(adapter);
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(
+                this,
+                R.layout.activity_my_list_item,
+                R.id.textView2,
+                data
+        );
+        ListView lstView = findViewById(R.id.lstItem1);
+        lstView.setAdapter(adapter1);
+    }
+
+    public String removeItem(ArrayList<String> data, int id ){
+        String i = data.get(id);
+        data.remove(id);
+        return i;
     }
 
     public void chuyenTrangDangNhap(View v){
